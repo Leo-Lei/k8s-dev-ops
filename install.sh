54ec418e9aad7f8313b68b2f7e9811525ea1d1db
雷威 <leiwei@dingdatech.com>
2017-10-26 14:32:20 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,10 @@ cp ${ROOT_DIR}/_config.yaml ${ROOT_DIR}.bak/_config.yaml
 
 rm -rf ${ROOT_DIR}
 
-wget https://codeload.github.com/Leo-Lei/k8s-dev-ops/zip/master -O /opt/k8s-dev-ops.zip
-unzip /opt/k8s-dev-ops.zip -d ROOT_DIR
-rm -rf /opt/k8s-dev-ops.zip
+mkdir -p ${ROOT_DIR}
+mkdir -p ${ROOT_DIR}.tmp
+wget https://codeload.github.com/Leo-Lei/k8s-dev-ops/zip/master -O ${ROOT_DIR}.bak/k8s-dev-ops.zip
+unzip ${ROOT_DIR}.bak/k8s-dev-ops.zip -d ${ROOT_DIR}.tmp
+cp -r ${ROOT_DIR}.tmp/k8s-dev-ops-master/* ${ROOT_DIR}
+rm -rf ${ROOT_DIR}.tmp
 
